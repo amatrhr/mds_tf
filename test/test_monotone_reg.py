759fb13050ba4980a28acd658a886c4959883ff4
C Laurin <cl14022@outlook.com>
2022-08-31 22:24:58 -0700

    Configuring tests, working out an ugly jacobian

--- test/test_monotone_reg.py
+++ test/test_monotone_reg.py
@@ -1,1 +1,13 @@
-# test with small synthetic data
+# test the KKT conditions with synthetic data (https://www.stat.umn.edu/geyer/8054/notes/isotonic.pdf)
+import pytest
+def test_stationary():
+    pass
+
+def test_primal_feas():
+    pass
+
+def test_dual_feas():
+    pass
+
+def test_complement_slack():
+    pass
